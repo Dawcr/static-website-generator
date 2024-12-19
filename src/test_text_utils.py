dd47e9a7db7e3dc1a45408c8b503d62ea2c32f59
dawcr <dawcraz@gmail.com>
2024-12-20 00:26:34 +0100

    added a way to get block type from block

--- src/test_text_utils.py
+++ src/test_text_utils.py
@@ -1,7 +1,9 @@
 import unittest
 
+from text_utils import BlockType
 from text_utils import (
     markdown_to_blocks,
+    block_to_block_type,
 )
 
 class TestMarkdownToBlocks(unittest.TestCase):
@@ -40,7 +42,7 @@ class TestMarkdownToBlocks(unittest.TestCase):
                 "This is a test for whitespaces",
                 "lots of whitespaces",
             ],
-            markdown_to_blocks(text)
+            markdown_to_blocks(text),
         )
         
     # boot.dev tests
@@ -87,7 +89,171 @@ This is the same paragraph on a new line
             ],
         )
         
-    
         
+class TestBlockToBlockType(unittest.TestCase):
+    def test_heading(self):
+        text = "#### This is a heading 4"
+        self.assertEqual(
+            block_to_block_type(text),
+            BlockType.HEADING,
+        )
+        
+    def test_code(self):
+        text = """```
+        This is a block of code
+        ```"""
+        self.assertEqual(
+            block_to_block_type(text),
+            BlockType.CODE,
+        )
+        
+        text2 = """```
+        This is a block of code
+        ``````
+        as is this abomination
+        ```"""
+        self.assertEqual(
+            block_to_block_type(text2),
+            BlockType.CODE,
+        )
+    
+    def test_quote(self):
+        text = """> quote
+>quote"""
+        self.assertEqual(
+            block_to_block_type(text),
+            BlockType.QUOTE,
+        )
+    
+    def test_ulist(self):
+        text = """- item
+- another item
+- yet another item"""
+        self.assertEqual(
+            block_to_block_type(text),
+            BlockType.UNORDERED_LIST,
+        )
+        
+        text = """* item
+* another item
+* yet another item"""
+        self.assertEqual(
+            block_to_block_type(text),
+            BlockType.UNORDERED_LIST,
+        )
+            
+    def test_ordered(self):
+        text = """1. this is an ordered list item
+2. this is another item"""
+        self.assertEqual(
+            block_to_block_type(text),
+            BlockType.ORDERED_LIST,
+        )
+            
+    def test_normal(self):
+        text = "this is just plain old text"
+        self.assertEqual(
+            block_to_block_type(text),
+            BlockType.PARAGRAPH,
+        )        
+        
+    # boot.dev tests
+    def test_block_to_block_types(self):
+        block = "# heading"
+        self.assertEqual(block_to_block_type(block), BlockType.HEADING)
+        block = "```\ncode\n```"
+        self.assertEqual(block_to_block_type(block), BlockType.CODE)
+        block = "> quote\n> more quote"
+        self.assertEqual(block_to_block_type(block), BlockType.QUOTE)
+        block = "* list\n* items"
+        self.assertEqual(block_to_block_type(block), BlockType.UNORDERED_LIST)
+        block = "1. list\n2. items"
+        self.assertEqual(block_to_block_type(block), BlockType.ORDERED_LIST)
+        block = "paragraph"
+        self.assertEqual(block_to_block_type(block), BlockType.PARAGRAPH)
+        
+        
+# class TestBlockToBlockType(unittest.TestCase):
+#     def test_heading(self):
+#         text = "#### This is a heading 4"
+#         self.assertEqual(
+#             block_to_block_type(text),
+#             BlockType.HEADING,
+#         )
+        
+#         text2 = "####### this is too many headings"
+#         with self.assertRaisesRegex(ValueError, "too many #, 7 provided"):
+#             block_to_block_type(text2)
+        
+#         text3 = "###$## contains an invalid character"
+#         with self.assertRaisesRegex(ValueError, "expecting only #, instead found "):
+#             block_to_block_type(text3)
+        
+#     def test_code(self):
+#         text = "```This is a block of code```"
+#         self.assertEqual(
+#             block_to_block_type(text),
+#             BlockType.CODE,
+#         )
+        
+#         text2 = "```This is a block of code``````as is this abomination```"
+#         self.assertEqual(
+#             block_to_block_type(text2),
+#             BlockType.CODE,
+#         )
+        
+#         text3 = "```This is a bad format for code``````it is missing a closure"
+#         with self.assertRaisesRegex(ValueError, "missing closing code delimiters"):
+#             block_to_block_type(text3)
+    
+#     def test_quote(self):
+#         text = """> quote
+#         >quote"""
+#         self.assertEqual(
+#             block_to_block_type(text),
+#             BlockType.QUOTE,
+#         )
+        
+#         text2 = """> to quote
+#         or not to quote"""
+#         with self.assertRaisesRegex(ValueError, "missing > character from start of line in quote block"):
+#             block_to_block_type(text2)
+    
+#     def test_ulist(self):
+#         text = """- item
+#         - another item
+#         - yet another item"""
+#         self.assertEqual(
+#             block_to_block_type(text),
+#             BlockType.UNORDERED_LIST,
+#         )
+        
+#         text2 = """- item
+#         * item2
+#         but not this"""
+#         with self.assertRaisesRegex(ValueError, "unordered list block must start with"):
+#             block_to_block_type(text2)
+            
+#     def test_ordered(self):
+#         text = """1. this is an ordered list item
+#         2. this is another item"""
+#         self.assertEqual(
+#             block_to_block_type(text),
+#             BlockType.ORDERED_LIST,
+#         )
+        
+#         text2 = """1. this is an ordered list item
+#         3. but this is not a correct order
+#         """
+#         with self.assertRaisesRegex(ValueError, "ordered list block must start with a number followed by a . character and a space"):
+#             block_to_block_type(text2)
+            
+#     def test_normal(self):
+#         text = "this is just plain old text"
+#         self.assertEqual(
+#             block_to_block_type(text),
+#             BlockType.PARAGRAPH,
+#         )
+    
 if __name__ == "__main__":
     unittest.main()
--- src/text_utils.py
+++ src/text_utils.py
@@ -1,3 +1,15 @@
+from enum import Enum
+
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    ORDERED_LIST = "ordered_list"
+    UNORDERED_LIST = "unordered_list"
+
+
 def markdown_to_blocks(markdown: str) -> list[str]:
     lines = markdown.split('\n')
     blocks: list[str] = []
@@ -16,3 +28,83 @@ def markdown_to_blocks(markdown: str) -> list[str]:
         blocks.append('\n'.join(block))
         
     return blocks
+
+
+def block_to_block_type(block: str) -> BlockType:
+    lines = block.split('\n')
+    
+    # Headings start with 1-6 # characters, followed by a space and then the heading text.
+    if block.startswith(("#", "##", "###", "####", "#####", "######", "#######")):
+        return BlockType.HEADING
+    
+    # Code blocks must start with 3 backticks and end with 3 backticks.
+    if len(lines) > 1 and block.startswith("```") and block.endswith("```"):
+        return BlockType.CODE
+    
+    # Every line in a quote block must start with a > character.
+    if block.startswith('>'):
+        for line in lines:
+            if not line.startswith('>'):
+                return BlockType.PARAGRAPH
+        return BlockType.QUOTE
+    
+    # Every line in an unordered list block must start with a * or - character, followed by a space.
+    if block.startswith(("* ", "- ")):
+        start = block[:2]
+        for line in lines:
+            if not line.startswith(start):
+                return BlockType.PARAGRAPH
+        return BlockType.UNORDERED_LIST
+    
+    # Every line in an ordered list block must start with a number followed by a . character and a space. The number must start at 1 and increment by 1 for each line.
+    if block.startswith("1. "):
+        for index, line in enumerate(lines, start=1):
+            if not line.startswith(f"{index}. "):
+                return BlockType.PARAGRAPH
+        return BlockType.ORDERED_LIST
+    
+    # If none of the above conditions are met, the block is a normal paragraph.
+    return BlockType.PARAGRAPH
+
+
+# def block_to_block_type(block: str) -> BlockType:
+#     match block[0]:
+#         # Headings start with 1-6 # characters, followed by a space and then the heading text.
+#         case '#':
+#             heading = block.split(maxsplit=1)[0]
+#             if set(heading) != set('#') or len(heading) > 6:
+#                 raise ValueError(f"too many #, {len(heading)} provided" if set(heading) == set('#')
+#                                  else f"expecting only #, instead found {set(heading) - set('#')}")
+#             return BlockType.HEADING
+#         # Code blocks must start with 3 backticks and end with 3 backticks.
+#         case '`':
+#             code = block.split("```")
+#             if len(code) % 2 != 1 or len(code) < 3:
+#                 raise ValueError("missing closing code delimiters")
+#             return BlockType.CODE
+#         # Every line in a quote block must start with a > character.
+#         case '>':
+#             quotes = block.split('\n')
+#             for quote in quotes:
+#                 if quote.strip()[0] != '>':
+#                     raise ValueError("missing > character from start of line in quote block")
+#             return BlockType.QUOTE
+#         # Every line in an unordered list block must start with a * or - character, followed by a space.
+#         case '*' | '-':
+#             u_list = block.split('\n')
+#             for item in u_list:
+#                 start = item.strip()[:2]
+#                 if not (start == "* " or start == "- "):
+#                     raise ValueError("unordered list block must start with a * or - character, followed by a space")
+#             return BlockType.UNORDERED_LIST
+#         # Every line in an ordered list block must start with a number followed by a . character and a space. The number must start at 1 and increment by 1 for each line.
+#         case '1':
+#             o_list = block.split('\n')
+#             for index, item in enumerate(o_list, start=1):
+#                 if item.strip()[:3] != f"{index}. ":
+#                     raise ValueError("ordered list block must start with a number followed by a . character and a space. The number must start at 1 and increment by 1 for each line")
+#             return BlockType.ORDERED_LIST
+#         # If none of the above conditions are met, the block is a normal paragraph.
+#         case _:
+#             return BlockType.PARAGRAPH
+        
